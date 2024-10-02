dab097d1305803a13190acad9f6ebcd45fc730ef
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-10-02 18:29:19 +0100

    files added

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,10 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: {
-      ...employeesList,
+      ...employeesList,  // Spread syntax to copy employeesList
     },
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
+    getNumberOfDepartments() {  // ES6 method property, no parameter needed
+      return Object.keys(employeesList).length;  // Use employeesList from outer scope
     },
-  },
-};
+  };
+}
