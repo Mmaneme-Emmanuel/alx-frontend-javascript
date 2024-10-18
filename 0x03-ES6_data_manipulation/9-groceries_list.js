12b91fa4a3089bbdb3efb7c0cd955e179a0c157e
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-10-18 19:37:46 +0100

    files added

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,5 +1,5 @@
 function groceriesList() {
-    const map = new Map(); // Use 'new Map()' to create a new Map instance
+    const map = new Map();
     map.set('Apples', 10);
     map.set('Tomatoes', 10);
     map.set('Pasta', 1);
